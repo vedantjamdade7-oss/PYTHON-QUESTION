9530e98a8884d9ec2404a34d1d3ae6576533a0c8
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-25 16:34:36 +0530

    Python Practice Question

--- Practice-2.py
+++ Practice-2.py
@@ -11,4 +11,15 @@ percentage = (total_marks/300)*100
 print(f" Name of Student : {name} ")
 print(f"percentage : {percentage:.2f}% will done!")
 
-# Q-2. write a python program that 
+# Q-2. write a python program that collects multiple types of data (e.g., name, age, height,and student status) from user input, stores them in a dictionary,and then print out the collection data?
+
+# initializing a Dictionary
+user_data = {}
+
+# input from user
+user_data['name'] = input("Enter your Full Name :")
+user_data['age'] = int(input("Enter your age :"))
+user_data['height'] = float(input("Enter your height :"))
+user_data['status'] = input("Enter your statues :")
+
+print(user_data)
