c5fa35b822bff947484675ba27c14308282d81e0
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-27 15:32:33 +0530

    Python Practice Question

--- Practice-4.py
+++ Practice-4.py
@@ -11,6 +11,11 @@ print("value of pi is {:.2f}".format(pi))
 # f-string
 print(f"pi value is {pi:.2f}")
 
+# Q-2. Extract charaters from index 2 to 8 with a step of 2: Given my_string ='Python course', slice charaters from index 2 to 8, skipping every other char
+
+mystring = "python course"
+
+print(mystring[2:8:2])
 
 
 # Get the substring that starts 4 characters from the end to the last character: for my_string = 'classification', slice the string starting from the 4th charater from the end to the charater.
