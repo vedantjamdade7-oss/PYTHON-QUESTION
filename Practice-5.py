c528cbb0771c16bbcbb8423d6e27faa82f13dcbb
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-01-30 22:15:04 +0530

    Python Practice Question

--- Practice-5.py
+++ Practice-5.py
@@ -7,3 +7,15 @@ while i < 4:
     # end is used for to create all variable in one line like,
     # (output = hey vedant 1  hey vedant 2  hey vedant 3 )
     # sep is used for seprated the value and any function for saprated
+
+# Q-2. Print star patterns - Using loops
+
+# star 
+
+n = 5
+
+for i in range (1,n+1):
+    for j in range (1,i+1):
+       print("*",end = " ")
+    print()
+
