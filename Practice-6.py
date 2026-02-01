f992449a5f3dac453160c89a46042d7a8b1e7d92
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-02-01 17:16:43 +0530

    Python Practice Question

--- Practice-6.py
+++ Practice-6.py
@@ -1,7 +1,88 @@
 # Q-1. Find the intersection (common element) of two lists
 
+# list , tuple, set and dict
+# data structures / collection
+
+
+list1 = [1,2,3,4,5]
+list2 = [4,5,6,7,8]
+
+def intersection_loop(lst1,lst2):
+    common_list = []
+    for item in lst1:
+        if item in lst2 and item not in common_list:
+            common_list.append(item)
+    return common_list
+# print(intersection_loop(list1,list2))
+
+# using list comprehension
+
+def intersection_comp (list1,list2): # def is a function 
+    return [item for item in list1 if item in list2] # item is just a variable name,item in lst1 checking the item[1,2,3,4],if in list2 
+print(intersection_comp(list1,list2))
+
 # Q-2. Find the most Frequent Elemnt in a list
+
+number = [1,2,2,3,3,3,4,4]
+
+def common_num (lst):
+    max_count = 0
+    most_freq = None
+    for item in lst:
+        count = lst.count(item)
+        if count > max_count:
+            max_count = count
+            most_freq = item
+    return most_freq
+   
+print(common_num(number))
 
 # Q-3. Find cumulative sum of a list.
 
+number = [1,2,3,4]
+
+def cumulative_sum(lst):
+    cum_sum = []
+    total = 0
+    for num in lst:
+        total += num
+        cum_sum.append(total)
+    return cum_sum
+print(cumulative_sum(number))
+
+
+cumulative = [  sum(number[:x + 1]) for x in range(len(number)) ]
+print(cumulative)
+
 # Q-4.  Remove Duplicates from a list - 2 Methodes
+
+# Methode-1
+
+fruits = ["apple", "banana", "cherry", "apple","banana"]
+
+# using loop
+# use in real word lack of list present there
+
+# def remove_duplicates(lst):
+#     unique = []
+#     seen = set()
+#     for item in lst:
+#         if item not in seen:
+#             unique.append(item)
+#             seen.add(item)
+#     return unique
+# print(remove_duplicates(fruits))
+
+# Methode-2
+
+def remove_duplicates(lst):
+    unique = []
+    for item in lst:
+        if item not in unique:
+            unique.append(item)
+       
+    return unique
+# print(remove_duplicates(fruits))
+
+# using set constructor
+print(list(set(fruits)))
