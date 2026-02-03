2847db877b3ac42f6bccbb464f12a1c4b7491d7a
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-02-03 10:56:54 +0530

    Python Practice Question

--- Practice-1.py
+++ Practice-1.py
@@ -5,6 +5,7 @@
 
 print('''Python is fun.\n "Quotes" and 'single Quotes' can be tricky''')
 
+
 # Q-2 Create 3 variables to store Name,age and city. Then print a sentence that uses these variables.
 
 name = "vedant"
@@ -15,3 +16,31 @@ print("My name is",name, "and my age is",age,"i am living in",city)
 #    OR
 
 print(f"my name is {name} and my age is {age} i am living in {city}") # this is f string 
+
+# Q-3. Write a program to print Twinkle Twinkle little star poem using in python.
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the trav'ller in the dark,
+Thanks you for your tiny spark,
+He could not see which way to go,
+If you did not twinkle so.
+
+In the dark blue sky you keep,
+And often thro' my curtains peep,
+For you never shut your eye,
+Till the sun is in the sky.
+
+'Tis your bright and tiny spark,
+Lights the trav'ller in the dark:
+Tho' I know not what you are,
+Twinkle, twinkle, little star.''')
+
