8d5e3fc73fc07167b34b2c68e3181a4e981d8b9a
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-02-06 05:43:10 +0530

    Python Practice Question

--- Practice-2.py
+++ Practice-2.py
@@ -123,3 +123,17 @@ s.add(int(n))
 
 print(s)
 
+# Q-8. Write a program to check if a comment is spam or not. A comment is considered spam if it contains any of the following phrases:"make a lot of money", "buy now", "subscribe this", "click this". The program should take a comment as input and print "This comment is a spam" otherwise print "This comment is not a spam".
+
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscribe this"
+p4 = "click this"
+
+message = input("Enter your comment:")
+ 
+if((p1 in message) or( p2 in message )or (p3 in message) or( p4 in message)):
+    print("This comment is a spam")
+
+else:
+    print("This comment is not a spam")
