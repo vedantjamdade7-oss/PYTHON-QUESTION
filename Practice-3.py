91d309f3a25aa0db403eed468ba4f4a242787784
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-02-07 22:43:43 +0530

    Python Practice Question

--- Practice-3.py
+++ Practice-3.py
@@ -82,4 +82,13 @@ for i in range (2,n):
         break
 else:
     print("Number is prime")
-    
+
+# Q-6. Factorial of a number: Write a program that takes a number as input and calculates its factorial.
+# 5! = 1 x 2 x 3 x 4 x 5
+
+n = int(input("Enter the number : "))
+product = 1
+for i in range(1,n+1):
+    product = product * i
+
+print(f"The factorial of {n} is {product}")
