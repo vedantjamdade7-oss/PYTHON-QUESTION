628abd35ec07cc86143a24dc310202b42ac6cf5d
My-Learning-Journey <jamdadevedant7@gmail.com>
2026-02-12 14:55:43 +0530

    Python Practice Question

--- Practice-7.py
+++ Practice-7.py
@@ -58,5 +58,29 @@ with open("this.txt") as f:
 
 with open("this_copy.txt","w") as f:
     f.write(Content)
+    
 
+import random
 
+def game():
+    print("You are playing the game..")
+    score = random.randint(1, 100)
+
+    # Fetch the hiscore
+    with open("hiscore.txt") as f:
+        hiscore = f.read()
+        if(hiscore!=""):
+            hiscore = int(hiscore)
+        else:
+            hiscore = 0
+
+    print(f"Your score: {score}")
+    if(score>hiscore):
+        # write this hiscore to the file
+        with open("hiscore.txt","w") as f:
+            f.write(str(score))
+
+    return score    
+
+game()    
+         
